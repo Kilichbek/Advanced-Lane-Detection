2e9384fbde672937a805e9ba69466aebc3fe731e
Kilichbek <kilich95@gmail.com>
2019-08-23 22:54:06 +0900

    Apply threshold techniques and color transform

--- calibrator.h
+++ calibrator.h
@@ -8,7 +8,7 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/calib3d/calib3d_c.h>
-#include <opencv2/imgproc/types_c.h>
+
 
 class CameraCalibrator {
 	//input points;
--- main.cpp
+++ main.cpp
@@ -2,10 +2,47 @@
 #include <filesystem>
 #include "calibrator.h"
 
-
 namespace fs = std::experimental::filesystem;
 using namespace std;
 
+void undistToHLS(cv::Mat& src,cv::Mat& dest, CameraCalibrator& calibrator)
+{
+	cv::Mat undist_img = calibrator.remap(src);
+	cv::cvtColor(undist_img, dest, cv::COLOR_BGR2HLS);
+	
+	return;
+}
+
+void absSobelThresh(cv::Mat& src, cv::Mat& dest, char orient = 'x', int kernel_size = 3, int thresh_min = 0, int thresh_max = 255)
+{
+	int dx, dy;
+	int ddepth = CV_32FC1;
+
+	cv::Mat grad_img, scaled;
+
+	if (orient == 'x') {
+		dy = 0;
+		dx = 1;
+	}
+	else {
+		dy = 1;
+		dx = 0;
+	}
+
+	cv::Sobel(src, grad_img, ddepth, dx, dy, kernel_size);
+	grad_img = cv::abs(grad_img);
+	
+	// Scaling 
+	double min, max;
+	cv::minMaxLoc(grad_img, &min, &max);
+	scaled = grad_img.mul(255 / max);
+	scaled.convertTo(scaled, CV_8UC1);
+
+	assert(scaled.type() == CV_8UC1);
+	cv::inRange(scaled, cv::Scalar(thresh_min), cv::Scalar(thresh_max), dest);
+
+	return;
+}
 int main()
 {
 	string path_to_files = "C:\\Users\\PC\\Documents\\CarND-Advanced-Lane-Lines-P4-master\\camera_cal";
@@ -49,14 +86,72 @@ int main()
 		first = false;
 	}
 
-	cv::Mat sample_img = cv::imread(images[0]);
-	cv::Mat win_mat(cv::Size(sample_img.size().width*2, sample_img.size().height), CV_8UC3);
-	cv::Mat undist_img = camCalibrator.remap(sample_img);
-	cv::Mat dest,undistHSV;
-	// hconcat(sample_img,undist_img, dest);
-	// Display big mat
-	//cv::cvtColor(undist_img, undistHSV, CV_BGR2RGB);
-	cv::imshow("Images",undist_img);
+	cv::Mat sample_img = cv::imread("C:\\Users\\PC\\Documents\\CarND-Advanced-Lane-Lines-P4-master\\test_images\\straight_lines1.jpg");
+
+	// undistort and convert to HLS color space
+	cv::Mat undist_hls;
+	undistToHLS(sample_img, undist_hls, camCalibrator);
+
+
+	// S channel of HLS space
+
+	cv::Mat hls_channels[3];
+	cv::split(undist_hls, hls_channels);
+
+	// absolute Sobel Threshold
+	cv::Mat sobel_x, sobel_y, combined;
+	absSobelThresh(hls_channels[2], sobel_x, 'x', 3, 10, 160);
+	absSobelThresh(hls_channels[2], sobel_y, 'y', 3, 10, 160);
+	cv::bitwise_and(sobel_x, sobel_y, combined); // combine gradient images
+	
+
+	// Perspective Transform
+
+	int x_size, y_size;
+	x_size = undist_hls.size().width;
+	y_size = undist_hls.size().height;
+
+	int bottom_y = 720;
+	int top_y = 425;
+
+	cv::Scalar color(0, 0, 255);
+	cv::Point left1(115, bottom_y), left2(560, top_y), right1(695, top_y), right2(1170, bottom_y);
+
+	int w = 2;
+	cv::Mat undistorted = camCalibrator.remap(sample_img);
+
+	cv::line(undistorted, left1, left2, color, w);
+	cv::line(undistorted, left2, right1, color, w);
+	cv::line(undistorted, right1, right2, color, w);
+	cv::line(undistorted, right2, left1, color, w);
+
+	cv::Mat gray, M, Minv, warped;
+	cv::cvtColor(undistorted, gray, cv::COLOR_BGR2GRAY);
+
+	int offset = 200, nX, nY;
+	nX = gray.size().width;
+	nY = gray.size().height;
+
+	cv::Point2f src[4], dst[4];
+
+	src[0] = left2;
+	src[1] = right1;
+	src[2] = right2;
+	src[3] = left1;
+
+	dst[0] = cv::Point2f(offset, 0);
+	dst[1] = cv::Point2f(nX - offset, 0);
+	dst[2] = cv::Point2f(nX - offset, nY);
+	dst[3] = cv::Point2f(offset, nY);
+
+	M = cv::getPerspectiveTransform(src, dst);
+	Minv = cv::getPerspectiveTransform(dst, src);
+	cv::warpPerspective(undistorted, warped, M, gray.size());
+
+	imshow("Undistorted", undistorted);
+	imshow("Perspective Transform", warped);
+
 	cv::waitKey(0);
+
 	return 0;
 }
